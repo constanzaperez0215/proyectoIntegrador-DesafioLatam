52d5cdbb92c3dbb8224829397715fe34d061ab5f
Juan Pablo M. <pablochileidh@gmail.com>
2024-09-07 22:29:03 -0600

    test actualiza usuario

--- Backend/src/test/server.test.jsx
+++ Backend/src/test/server.test.jsx
@@ -7,3 +7,19 @@ describe('CRUD', () => {
     expect(res.status).toBe(200)
   })
 })
+
+describe('CRUD', () => {
+  test('[PUT /users/:id] actualiza un usuario y devuelve 200 status', async () => {
+    const userId = '1' // Asegúrate de que este ID exista en tu base de datos
+    const updatedUser = {
+      name: 'Juan Pérez Actualizado'
+    };
+
+    const res = await request(server)
+      .put(`/users/${userId}`)
+      .send(updatedUser)
+
+    expect(res.status).toBe(200)
+    expect(res.body).toHaveProperty('name', updatedUser.name)
+  });
+});
